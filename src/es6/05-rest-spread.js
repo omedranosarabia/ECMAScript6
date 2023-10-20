5bb0845c2ead7357931c9ddab4c8b6e2af8cb3ac
Omar Medrano Sarabia <omedrano@bb.com.mx>
2023-10-20 10:50:55 -0600

    ES6: spread operator

--- src/es6/05-rest-spread.js
+++ src/es6/05-rest-spread.js
@@ -10,6 +10,24 @@ console.log(first, second, rest);
 
 let user = { username: 'Ricardo', age: 23, country: 'MX' };
 
-let { username, age, country } = user;
+let { username, age } = user;
 
-console.log(username, age, country);
+console.log(username, age);
+
+// spread operator
+
+let person = { name: 'Ricardo', age: 23 };
+
+let country = { country: 'MX' };
+
+let data = { id: 1, ...person, ...country };
+
+console.log(data);
+
+// rest operator
+
+function sum(num, ...values) {
+    return num + values.reduce((a, b) => a + b, 0);
+}
+
+console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
